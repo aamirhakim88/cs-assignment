984c078e3e3e997986abd66b14c6da0a50f6742d
Aamir Hakim <aamirhakim88@gmail.com>
2019-05-21 19:03:24 +0530

    Update to Deployer Script

--- deployer.sh
+++ deployer.sh
@@ -1,33 +1,30 @@
 # !/usr/bin/env bash
 
-BUCKET_NAME="cs-cfn-nested-bucket"
-STACK_NAME="cs-assignment-stack"
+if [[ $1 != "" ]]; then
+  $BUCKET_NAME=$1
+else
+  echo "Bucket Name is Blank"
+  exit 0
+fi
 
-# if [[ $1 != "" ]]; then
-#   $BUCKET_NAME=$1
-# else
-#   echo "Bucket Name is Blank"
-#   exit 0
-# fi
-#
-# if [[ $2 != "" ]]; then
-#   $STACK_NAME=$2
-# else
-#   echo "Stack Name is Blank"
-#   exit 0
-# fi
-#
-# if [[ $3 != "" ]]; then
-#   PROFILE_CHECK=$([[ $(aws configure --profile $3 list) && $? -eq 0 ]] && echo 1 || echo 0)
-# else
-#   PROFILE_CHECK=0
-# fi
-#
-# if [[ $PROFILE_CHECK == 1 ]]; then
-#   export AWS_DEFAULT_PROFILE=$3
-# else
-#   export AWS_DEFAULT_PROFILE="default"
-# fi
+if [[ $2 != "" ]]; then
+  $STACK_NAME=$2
+else
+  echo "Stack Name is Blank"
+  exit 0
+fi
+
+if [[ $3 != "" ]]; then
+  PROFILE_CHECK=$([[ $(aws configure --profile $3 list) && $? -eq 0 ]] && echo 1 || echo 0)
+else
+  PROFILE_CHECK=0
+fi
+
+if [[ $PROFILE_CHECK == 1 ]]; then
+  export AWS_DEFAULT_PROFILE=$3
+else
+  export AWS_DEFAULT_PROFILE="default"
+fi
 
 AZs=($(aws ec2 describe-availability-zones --output json | jq -r '.AvailabilityZones[].ZoneName'))
 
